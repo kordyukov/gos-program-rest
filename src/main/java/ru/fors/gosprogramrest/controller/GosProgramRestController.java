4de5c938e9b924940cbee064bb603695502cf067
Denis <mrkordyukov@gmail.com>
2023-08-29 15:32:24 +0300

    @

--- src/main/java/ru/fors/gosprogramrest/controller/GosProgramRestController.java
+++ src/main/java/ru/fors/gosprogramrest/controller/GosProgramRestController.java
@@ -1,7 +1,9 @@
 package ru.fors.gosprogramrest.controller;
 
+import com.google.gson.JsonObject;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.String.valueOf;
+
 @Slf4j
 @RestController
 @RequestMapping("/portal_gp_api")
@@ -25,21 +29,21 @@ public class GosProgramRestController {
     private String fileResponse;
 
     @GetMapping("/v2/{year}")
-    public ResponseEntity<String> getProgramAndFinanceList(@PathVariable("year") Integer year) throws IOException, ClassNotFoundException {
+    public ResponseEntity<JSONObject> getProgramAndFinanceList(@PathVariable("year") Integer year) throws IOException, ClassNotFoundException {
 
         Map<Object, Object> objectObjectMap = updateFieldsService.receivedFields(year);
 
         List<String> keysFromFileByName = updateFieldsService.getKeysFromFileByName(fileResponse);
 
-        Map<Object, Object> response = new HashMap<>();
+        Map<String, Object> response = new HashMap<>();
 
         objectObjectMap.keySet().forEach(key -> {
             for (String responseField : keysFromFileByName) {
-                if (key.equals(responseField)) response.put(key, objectObjectMap.get(key));
+                if (key.equals(responseField)) response.put(valueOf(key), objectObjectMap.get(key));
             }
         });
 
-        return ResponseEntity.ok(response.toString());
+        return ResponseEntity.ok(new JSONObject(response));
     }
 
 }
